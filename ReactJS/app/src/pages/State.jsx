b3e1773ec240f92f31bb16debc013ce0e8bfb800
VISHNU M K <futuralabs.mern@gmail.com>
2025-08-04 12:17:02 +0530

    multiple state values

--- ReactJS/app/src/pages/State.jsx
+++ ReactJS/app/src/pages/State.jsx
@@ -17,16 +17,41 @@ import { useState } from "react"
 
 const State = () => {
 
-    const [counter, setCounter] = useState(0);
+    // const [counter, setCounter] = useState(0);
 
-    const handleUpdate = () => {
-        setCounter(state => state + 1);
+    // const handleUpdate = () => {
+    //     setCounter(state => state + 1);
+    // }
+
+    // return <div>
+    //     <h2>State</h2>
+    //     <p>value: {counter}</p>
+    //     <button onClick={handleUpdate}>Update</button>
+    // </div>
+
+    const [counters, setCounters] = useState({ one: 0, two: 0 });
+
+    const IncrementOne = () => {
+        setCounters({ ...counters, one: counters.one + 1 });
+    }
+
+    const IncrementTwo = () => {
+        setCounters({ ...counters, two: counters.two + 1 });
     }
 
     return <div>
-        <h2>State</h2>
-        <p>value: {counter}</p>
-        <button onClick={handleUpdate}>Update</button>
+        <div>
+            <p>Counter 1: {counters.one}</p>
+            <div>
+                <button onClick={IncrementOne}>Increment</button>
+            </div>
+        </div>
+        <div>
+            <p>Counter 2: {counters.two}</p>
+            <div>
+                <button onClick={IncrementTwo}>Increment</button>
+            </div>
+        </div>
     </div>
 }
 
